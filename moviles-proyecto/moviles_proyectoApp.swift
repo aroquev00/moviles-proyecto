d840f7b4022dd003d5f1bc38eef6ee545116ba4c
aroquev00 <aroquev00@users.noreply.github.com>
2021-03-19 14:27:00 -0600

    Add starter project

---
+++ moviles-proyecto/moviles_proyectoApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviles_proyectoApp.swift
+//  moviles-proyecto
+//
+//  Created by Armando Roque Rodriguez on 19/03/21.
+//
+
+import SwiftUI
+
+@main
+struct moviles_proyectoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
