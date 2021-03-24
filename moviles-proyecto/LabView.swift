e39d62ffcd33701896503202da301eb3b8ea4fd7
aroquev00 <aroquev00@users.noreply.github.com>
2021-03-24 11:54:27 -0600

    Add rotation example in lab

--- moviles-proyecto/LabView.swift
+++ moviles-proyecto/LabView.swift
@@ -10,9 +10,15 @@ import SwiftUI
 struct LabView: View {
     @Environment(\.presentationMode) var presentationMode
     
+    @State var rotation: Double = 0.0
+    
     var body: some View {
         ZStack {
             VStack {
+                Text("Watch me rotate")
+                    .rotationEffect(.degrees(rotation))
+                    .padding()
+                Slider(value: $rotation, in: -90...90)
                 Text("Aquí va el lab")
                     .padding()
                 Button("Dismiss Me") {
