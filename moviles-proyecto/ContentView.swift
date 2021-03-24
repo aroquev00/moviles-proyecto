d85b20d19c7313a80f5eb8a31a94dccce02d6bee
aroquev00 <aroquev00@users.noreply.github.com>
2021-03-23 22:41:41 -0600

    Change orientation to horizontal

--- moviles-proyecto/ContentView.swift
+++ moviles-proyecto/ContentView.swift
@@ -16,6 +16,18 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Landscape {
+            ContentView()
+        }
+        
+    }
+}
+
+struct Landscape<Content>: View where Content: View {
+    let content: () -> Content
+    let height = UIScreen.main.bounds.width //toggle width height
+    let width = UIScreen.main.bounds.height
+    var body: some View {
+        content().previewLayout(PreviewLayout.fixed(width: width, height: height))
     }
 }
