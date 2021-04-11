cc8522003cc5fe988e254a7ab74114e95b5d954e
aroquev00 <aroquev00@users.noreply.github.com>
2021-04-11 16:36:09 -0500

    View sprites contributions to balancer

--- moviles-proyecto/CalculationsView.swift
+++ moviles-proyecto/CalculationsView.swift
@@ -33,11 +33,73 @@ struct CalculationsView: View {
                     .background(Color(.red))
                 
                 // Monitos
+                // Lado Izquierdo
+                VStack {
+                    Text("Lado izquierdo")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.red)
+                    ForEach(simulator.spots, id: \.self) { spot in
+                        if !spot.side && spot.sprite != nil {
+                            spriteDataView(spot: spot)
+                        }
+                    }
+                }
+                
+                // Lado Derecho
+                VStack {
+                    Text("Lado derecho")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.blue)
+                    ForEach(simulator.spots, id: \.self) { spot in
+                        if spot.side && spot.sprite != nil {
+                            spriteDataView(spot: spot)
+                        }
+                    }
+                }
                 
             }
         }
         
         
+    }
+    
+    
+}
+
+struct spriteDataView: View {
+    var spot: SimulatorSpot
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(spot.sprite!.name)
+                Text(spriteInfo1(spot: spot))
+                Text(spriteInfo2(spot: spot))
+                Text(spriteInfo3(spot: spot))
+            }
+            Spacer()
+            Image(uiImage: spot.sprite!.image!)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 100)
+            Spacer()
+        }
+    }
+    
+    func spriteInfo1(spot: SimulatorSpot) -> String {
+        let sprite = spot.sprite!
+        return "Masa: \(sprite.weight) kg. - Distancia del centro: \(spot.distance) m."
+    }
+    
+    func spriteInfo2(spot: SimulatorSpot) -> String {
+        let sprite = spot.sprite!
+        return "Torca ejercida = (\(sprite.weight) kg.) x (9.81 m/s\u{00B2}) x (\(spot.distance) m.)"
+    }
+    
+    func spriteInfo3(spot: SimulatorSpot) -> String {
+        let sprite = spot.sprite!
+        let torque = sprite.weight * 9.81 * spot.distance
+        return "Torca ejercida = \(String(format: "%.2f", torque)) Nm"
     }
 }
 
