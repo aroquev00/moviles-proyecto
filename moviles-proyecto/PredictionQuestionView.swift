cf3bc799a4e530f1eb0fa25b48bf553ed3201295
marbrocu <56001916+marbrocu@users.noreply.github.com>
2021-04-28 12:18:54 -0700

    Alertaspara prediccion
    se puso alerta para la prediccion y cambiar de pagina. se pusieron variables auxiliares en el metodo de add points(lo cual puede no ser lo mejor)

--- moviles-proyecto/PredictionQuestionView.swift
+++ moviles-proyecto/PredictionQuestionView.swift
@@ -7,8 +7,14 @@
 
 import SwiftUI
 
+enum ActiveAlert{
+    case first, second
+}
+
 struct PredictionQuestionView: View {
     @State var question: PredictionQuestion
+    @State var alertVisible: Bool = false
+    @State var activeAlert: ActiveAlert = .first
     @Binding var quiz: Quiz
     
     var body: some View {
@@ -25,7 +31,13 @@ struct PredictionQuestionView: View {
                     getPredictionButton(text: "Se inclina a la izquierda", swivel: .left)
                     getPredictionButton(text: "Se queda nivelado", swivel: .equilibrium)
                     getPredictionButton(text: "Se inclina a la derecha", swivel: .right)
-                }
+                }.alert(isPresented: $alertVisible, content: {
+                    switch activeAlert{
+                    case .first:
+                        return Alert(title: Text( "Incorrecto"), message: Text("Avanza a la siguiente pregunta o intentalo de nuevo, Tu puedes!"), primaryButton: .default(Text("Retry")), secondaryButton: .default(Text("Next"), action: { quiz.nextQuestion()} ))
+                    case .second:
+                        return Alert(title: Text("Correcto!"), message: Text("Avanza a la siguiente pregunta"), dismissButton: .default(Text("Next"), action: { quiz.nextQuestion()} ))
+                    }})
                 switch question.answerStatus {
                 case AnswerStatus.correct:
                     Text("Answer status: Correct")
@@ -39,6 +51,10 @@ struct PredictionQuestionView: View {
         }
     }
     
+    func getAlert(){
+        
+    }
+    
     func getPredictionButton(text: String, swivel: Swivel) -> some View {
         return Button(text) {
             question.selectedAnswer = swivel
@@ -50,6 +66,12 @@ struct PredictionQuestionView: View {
     func addPoints() {
         if question.answerStatus == .correct {
             quiz.points += 1
+            alertVisible = true
+            activeAlert = .second
+        }
+        else if question.answerStatus == .incorrect {
+            alertVisible = true
+            activeAlert = .first
         }
     }
 }
