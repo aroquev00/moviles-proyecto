0058f749882c7a72b4a14b5baaeb964e66b34e47
aroquev00 <aroquev00@users.noreply.github.com>
2021-05-10 20:38:15 -0500

    Add sprite factory

---
+++ moviles-proyecto/SpriteFactory.swift
@@ -0,0 +1,19 @@
+//
+//  SpriteFactory.swift
+//  moviles-proyecto
+//
+//  Created by Armando Roque Rodriguez on 10/05/21.
+//
+
+struct SpriteFactory {
+    static var labRowSprites = [
+        Sprite(name: "Mario", weight: 20, height: 1, imageURL: "mario"),
+        Sprite(name: "Kirby", weight: 10, height: 0.4, imageURL: "kirby"),
+        Sprite(name: "Steve", weight: 30, height: 1, imageURL: "steve"),
+        Sprite(name: "Master Chief", weight: 80, height: 1.0, imageURL: "masterchief"),
+        Sprite(name: "Plankton", weight: 5, height: 0.5, imageURL: "plankton"),
+        Sprite(name: "Sonic", weight: 15, height: 0.6, imageURL: "sonic"),
+        Sprite(name: "Link", weight: 60, height: 0.85, imageURL: "link"),
+        Sprite(name: "Megaman", weight: 20, height: 1, imageURL: "megaman")
+    ]
+}
