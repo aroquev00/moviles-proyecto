3b78c6254b0790598debc56819ac1098c6ef72cc
aroquev00 <aroquev00@users.noreply.github.com>
2021-05-11 23:14:20 -0500

    Calcs view

--- moviles-proyecto/CalculationsView.swift
+++ moviles-proyecto/CalculationsView.swift
@@ -27,153 +27,123 @@ struct CalculationsView: View {
     var body: some View {
         GeometryReader { mainGeo in
             ZStack {
+                Color.blue
+                    //.resizable()
+                    .edgesIgnoringSafeArea(.all)
                 ScrollView {
                     VStack {
                         GeometryReader { geo in
                             HStack(spacing: 10) {
+                                Spacer()
                                 Button {
                                     // Return to lab view
-                                    print(simulator.spots)
                                     presentationMode.wrappedValue.dismiss()
                                 } label: {
                                     Image(systemName: "arrowshape.turn.up.left.fill")
                                         .resizable()
-                                        .frame(width: geo.size.width * 0.05, height: geo.size.height * 0.4, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                        .frame(width: geo.size.width * 0.05, height: geo.size.height * 0.4)
                                         .foregroundColor(Color(#colorLiteral(red: 1, green: 0.6, blue: 0.07843137255, alpha: 1)))
                                 }
                                 Text("Cálculos para el problema")
                                     .font(Font.custom("Bangers-Regular", size: geo.size.width * 0.06))
                                     .tracking(5)
-                                    .frame(width: geo.size.width * 0.9, height: geo.size.height, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                    .frame(width: geo.size.width, height: geo.size.height)
                                     .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                             }
-                            .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
                             .background(Color(#colorLiteral(red: 0.231372549, green: 0.1568627451, blue: 0.8, alpha: 1)))
                         }
-                        .frame(width: mainGeo.size.width, height: mainGeo.size.height * 0.2, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .frame(width: mainGeo.size.width, height: mainGeo.size.height * 0.2)
                         
                         // Monitos
                         // Lado Izquierdo
-                        GeometryReader { geo in
-                            VStack {
-                                Text("Lado izquierdo")
-                                    .font(Font.custom("Bangers-Regular", size: geo.size.width * 0.04))
-                                    .frame(width: geo.size.width * 0.3, height: geo.size.height * 0.7, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                    .background(Color(#colorLiteral(red: 0.01960784314, green: 0.4039215686, blue: 0.9098039216, alpha: 1)))
-                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                    .cornerRadius(10)
-                                if leftSpots.count > 0 {
-                                    ForEach(leftSpots, id: \.self) { spot in
-                                        
-                                        spriteDataView(spot: spot)
-                                        
-                                    }
-                                } else {
-                                    Text("No se pusieron personajes del lado izquierdo")
-                                        .font(.system(size: geo.size.width * 0.025))
-                                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                        VStack {
+                            getSectionTitle(title: "Lado izquierdo", foregroundColor: Color(#colorLiteral(red: 0.01960784314, green: 0.4039215686, blue: 0.9098039216, alpha: 1)), geo: mainGeo)
+                            
+                            if leftSpots.count > 0 {
+                                ForEach(leftSpots, id: \.self) { spot in
+                                    spriteDataView(spot: spot, geo: mainGeo)
                                 }
+                            } else {
+                                Text("No se pusieron personajes del lado izquierdo")
+                                    .font(.system(size: mainGeo.size.width * 0.025))
+                                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
                             }
-                            .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
                         }
-                        .frame(width: mainGeo.size.width, height: mainGeo.size.height * 0.15, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                         
                         // Lado Derecho
-                        GeometryReader { geo in
-                            VStack {
-                                Text("Lado derecho")
-                                    .font(Font.custom("Bangers-Regular", size: geo.size.width * 0.04))
-                                    .frame(width: geo.size.width * 0.3, height: geo.size.height * 0.7, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                    .background(Color(#colorLiteral(red: 0.1490196078, green: 0.6784313725, blue: 0.6784313725, alpha: 1)))
-                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                    .cornerRadius(10)
-                                if rightSpots.count > 0 {
-                                    ForEach(rightSpots, id: \.self) { spot in
-                                        
-                                        spriteDataView(spot: spot)
-                                        
-                                    }
-                                } else {
-                                    Text("No se pusieron personajes del lado derecho")
-                                        .font(.system(size: geo.size.width * 0.025))
-                                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                        VStack {
+                            getSectionTitle(title: "Lado derecho", foregroundColor: Color(#colorLiteral(red: 0.1490196078, green: 0.6784313725, blue: 0.6784313725, alpha: 1)), geo: mainGeo)
+                            if rightSpots.count > 0 {
+                                ForEach(rightSpots, id: \.self) { spot in
+                                    spriteDataView(spot: spot, geo: mainGeo)
                                 }
-                                
+                            } else {
+                                Text("No se pusieron personajes del lado derecho")
+                                    .font(.system(size: mainGeo.size.width * 0.025))
+                                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
                             }
-                            .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
                         }
-                        .frame(width: mainGeo.size.width, height: mainGeo.size.height * 0.15, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                         
                         // Resultado final
-                        GeometryReader { geo in
-                            VStack(spacing: 5) {
-                                Text("Cálculo final")
-                                    .font(Font.custom("Bangers-Regular", size: geo.size.width * 0.04))
-                                    .frame(width: geo.size.width * 0.3, height: geo.size.height * 0.4, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                    .background(Color(#colorLiteral(red: 0.8509803922, green: 0.007843137255, blue: 0.007843137255, alpha: 1)))
-                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                    .cornerRadius(10)
+                        VStack(spacing: 5) {
+                            getSectionTitle(title: "Cálculo final", foregroundColor: Color(#colorLiteral(red: 0.8509803922, green: 0.007843137255, blue: 0.007843137255, alpha: 1)), geo: mainGeo)
 
-                                
-                                // Left torque
-                                let leftData = sideTorqueTotal(spots: leftSpots)
-                                if leftSpots.count != 0 {
-                                    Text("Torca izquierda = \(leftData.names!)")
-                                        .font(.system(size: geo.size.width * 0.025))
-                                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                                    if leftSpots.count > 1 {
-                                        Text("Torca izquierda = \(leftData.torques!)")
-                                            .font(.system(size: geo.size.width * 0.025))
-                                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                                            .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                                    }
-                                }
-                                Text("Torca izquierda = \(leftData.totalTorque)")
-                                    .font(.system(size: geo.size.width * 0.025))
-                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            // Left torque
+                            let leftData = sideTorqueTotal(spots: leftSpots)
+                            if leftSpots.count != 0 {
+                                Text("Torca izquierda = \(leftData.names!)")
+                                    .font(.system(size: mainGeo.size.width * 0.025))
+                                    .fontWeight(.bold)
                                     .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                                
-                                
-                                // Right torque
-                                let rightData = sideTorqueTotal(spots: rightSpots)
-                                if rightSpots.count != 0 {
-                                    Text("Torca derecha = \(rightData.names!)")
-                                        .font(.system(size: geo.size.width * 0.025))
-                                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                                    if rightSpots.count > 1 {
-                                        Text("Torca derecha = \(rightData.torques!)")
-                                            .font(.system(size: geo.size.width * 0.025))
-                                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                                            .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                                        
-                                    }
-                                }
-                                Text("Torca derecha = \(rightData.totalTorque)")
-                                    .font(.system(size: geo.size.width * 0.025))
-                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-            
-                                // Final result
-                                if simulator.totalTorque == 0 {
-                                    Text("Como las torcas izquierda y derecha son iguales, la tabla se mantiene en equilibrio.")
-                                        .font(.system(size: geo.size.width * 0.025))
-                                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                                } else if (simulator.totalTorque > 0) {
-                                    Text("Como la torca derecha es mayor, se ladea hacia la derecha.")
-                                        .font(.system(size: geo.size.width * 0.025))
-                                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                                } else {
-                                    Text("Como la torca izquierda es mayor, se ladea hacia la izquierda.")
-                                        .font(.system(size: geo.size.width * 0.025))
+                                if leftSpots.count > 1 {
+                                    Text("Torca izquierda = \(leftData.torques!)")
+                                        .font(.system(size: mainGeo.size.width * 0.025))
+                                        .fontWeight(.bold)
                                         .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
                                 }
-                                
                             }
-                            .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
+                            Text("Torca izquierda = \(leftData.totalTorque)")
+                                .font(.system(size: mainGeo.size.width * 0.025))
+                                .fontWeight(.bold)
+                                .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                            
+                            
+                            // Right torque
+                            let rightData = sideTorqueTotal(spots: rightSpots)
+                            if rightSpots.count != 0 {
+                                Text("Torca derecha = \(rightData.names!)")
+                                    .font(.system(size: mainGeo.size.width * 0.025))
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                                if rightSpots.count > 1 {
+                                    Text("Torca derecha = \(rightData.torques!)")
+                                        .font(.system(size: mainGeo.size.width * 0.025))
+                                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                                    
+                                }
+                            }
+                            Text("Torca derecha = \(rightData.totalTorque)")
+                                .font(.system(size: mainGeo.size.width * 0.025))
+                                .fontWeight(.bold)
+                                .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+        
+                            // Final result
+                            if simulator.totalTorque == 0 {
+                                Text("Como las torcas izquierda y derecha son iguales, la tabla se mantiene en equilibrio.")
+                                    .font(.system(size: mainGeo.size.width * 0.025))
+                                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                            } else if (simulator.totalTorque > 0) {
+                                Text("Como la torca derecha es mayor, se ladea hacia la derecha.")
+                                    .font(.system(size: mainGeo.size.width * 0.025))
+                                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                            } else {
+                                Text("Como la torca izquierda es mayor, se ladea hacia la izquierda.")
+                                    .font(.system(size: mainGeo.size.width * 0.025))
+                                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                            }
+                            
                         }
-                        .frame(width: mainGeo.size.width, height: mainGeo.size.height * 0.35, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                     }
                 }
             }
@@ -209,13 +179,26 @@ struct CalculationsView: View {
             return (names, torques, String(format: "%.2f", totalTorque) + " Nm")
         }
     }
+    
+    // Function that returns the Text with the title for the section
+    func getSectionTitle(title: String, foregroundColor: Color, geo: GeometryProxy) -> some View {
+        Text(title)
+            .font(Font.custom("Bangers-Regular", size: geo.size.width * 0.04))
+            .frame(width: geo.size.width * 0.3, height: geo.size.height * 0.105)
+            .background(foregroundColor)
+            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+            .cornerRadius(10)
+    }
 }
 
 struct spriteDataView: View {
     var spot: SimulatorSpot
+    var geo: GeometryProxy
     
     var body: some View {
         HStack {
+            Spacer()
+                .frame(width: geo.size.width * 0.05)
             VStack(alignment: .leading) {
                 Text(spot.sprite!.name)
                 Text(spriteInfo1(spot: spot))
