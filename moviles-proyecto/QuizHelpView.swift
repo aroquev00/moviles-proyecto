70c5b7437f7f99076e73dbbb1ba0e9931e287c8d
aroquev00 <aroquev00@users.noreply.github.com>
2021-05-27 00:08:29 -0500

    Fix help text

--- moviles-proyecto/QuizHelpView.swift
+++ moviles-proyecto/QuizHelpView.swift
@@ -15,7 +15,8 @@ struct QuizHelpView: View {
     //Array with the 3 possible message outputs
     let messages = ["Calcula la torca resultante según los personajes que están en la tabla y presiona el botón que represente cómo quedaría si se quitan los soportes.",
                     "Primero, balancea la tabla colocando al personaje que se te da en el lugar que le corresponda.\nDespúes, realiza los cálculos necesarios para despejar la masa del personaje especificado.\nReporta el resultado utilizando el slider que se encuentra debajo de la tabla.",
-                    "Haz los cálculos necesarios para saber en qué posición debe ir el personaje indicado para que la tabla esté balanceada si se quitan los soportes. Es decir, que la torca resultante sea 0."]
+                    "Haz los cálculos necesarios para saber en qué posición debe ir el personaje indicado para que la tabla esté balanceada si se quitan los soportes.\nEs decir, que la torca resultante sea 0."
+    ]
     
     var body: some View {
         GeometryReader { mainGeo in
@@ -26,52 +27,48 @@ struct QuizHelpView: View {
                 
                 ScrollView {
                     VStack {
-                        GeometryReader { geo in
-                            //Bar at the top
-                            HStack() {
-                                Spacer()
-                                //Back button
-                                Button {
-                                    // Dismiss view
-                                    presentationMode.wrappedValue.dismiss()
-                                } label: {
-                                    Image(systemName: "arrowshape.turn.up.left.fill")
-                                        .resizable()
-                                        .frame(width: geo.size.width * 0.05, height: geo.size.height * 0.4)
-                                        .foregroundColor(Color.mainButtonBackground)
-                                }
-                                //Title text
-                                Text("Ayuda")
-                                    .font(Font.custom("Bangers-Regular", size: geo.size.width * 0.06))
-                                    .tracking(5)
-                                    .frame(width: geo.size.width, height: geo.size.height)
-                                    .foregroundColor(.mainTitleTextForeground)
+                        //Bar at the top
+                        HStack() {
+                            Spacer()
+                            //Back button
+                            Button {
+                                // Dismiss view
+                                presentationMode.wrappedValue.dismiss()
+                            } label: {
+                                Image(systemName: "arrowshape.turn.up.left.fill")
+                                    .resizable()
+                                    .frame(width: mainGeo.size.width * 0.05, height: mainGeo.size.height * 0.08)
+                                    .foregroundColor(Color.mainButtonBackground)
                             }
-                            .background(Color.mainTitleBackground)
+                            //Title text
+                            Text("Ayuda")
+                                .font(Font.custom("Bangers-Regular", size: mainGeo.size.width * 0.06))
+                                .tracking(5)
+                                .frame(width: mainGeo.size.width, height: mainGeo.size.height *  0.2)
+                                .foregroundColor(.mainTitleTextForeground)
                         }
-                        .frame(width: mainGeo.size.width, height: mainGeo.size.height * 0.2)
-                        
+                        .background(Color.mainTitleBackground)
+
                         Spacer()
                         
-                        //Help text
-                        HStack {
-                            if question.questionType == .prediction {
-                                Text(messages[0])
-                                    .font(.system(size: mainGeo.size.width * 0.05))
-                                    .foregroundColor(Color.mainTextForeground)
-                            }
-                            else if question.questionType == .massEstimation {
-                                Text(messages[1])
-                                    .font(.system(size: mainGeo.size.width * 0.05))
-                                    .foregroundColor(Color.mainTextForeground)
-                            }
-                            else {
-                                Text(messages[2])
-                                    .font(.system(size: mainGeo.size.width * 0.05))
-                                    .foregroundColor(Color.mainTextForeground)
-                            }
+                        if question.questionType == .prediction {
+                            Text(messages[0])
+                                .font(.system(size: mainGeo.size.width * 0.05))
+                                .foregroundColor(Color.mainTextForeground)
+                                //.multilineTextAlignment(.leading)
                         }
-                        .frame(width: mainGeo.size.width * 0.95, alignment: .topLeading)
+                        else if question.questionType == .massEstimation {
+                            Text(messages[1])
+                                .font(.system(size: mainGeo.size.width * 0.05))
+                                .foregroundColor(Color.mainTextForeground)
+                                //.multilineTextAlignment(.leading)
+                        }
+                        else {
+                            Text(messages[2])
+                                .font(.system(size: mainGeo.size.width * 0.05))
+                                .foregroundColor(Color.mainTextForeground)
+                                .lineLimit(nil)
+                        }
                     }
                 }
             }
