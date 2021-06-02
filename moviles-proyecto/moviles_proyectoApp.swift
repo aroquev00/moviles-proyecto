4141b19147759a44fb86e2cf90563ee161572f1a
aroquev00 <aroquev00@users.noreply.github.com>
2021-06-02 16:44:42 -0500

    Work on delegate

--- moviles-proyecto/moviles_proyectoApp.swift
+++ moviles-proyecto/moviles_proyectoApp.swift
@@ -9,8 +9,6 @@ import SwiftUI
 import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    static var orientationLock = UIInterfaceOrientationMask.landscape
-    
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return .landscape
     }
